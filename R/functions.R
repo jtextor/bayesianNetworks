6a9c8d5f296da6c941db9199f439d2a401aee3d2
Johannes Textor <j.textor@science.ru.nl>
2019-09-09 16:55:15 +0200

    function now public

--- R/functions.R
+++ R/functions.R
@@ -2,11 +2,15 @@
 #'
 #' @param N number of data points to generate.
 #' @return Returns a data frame containing the simulated data.
+#' @export
 simulate.sprinkler <- function( N = 1000 ){
-	data.frame(
-		Cloudy = rep(0,N),
-		Sprinkler = rep(0,N),
-		Rain = rep(0,N),
-		WetGrass = rep(0,N)
-	)
+	p <- c(0.198, 0.002, 0.005, 0.045, 0.02, 0.18, 5e-04, 0.0495, 0.0891,  
+		9e-04, 0.036, 0.324, 0.001, 0.009, 4e-04, 0.0396)
+	d <- expand.grid( WetGrass=0:1, Rain=0:1, Sprinkler=0:1, Cloudy=0:1 )
+	s <- sample( 1:nrow(d), N, replace=TRUE, prob=p )
+	d <- d[s,]
+	rownames(d) <- NULL
+	d
 }
+
+
